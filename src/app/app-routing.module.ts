4a84e02964177df4f71572eb5123b9cd3c8094bd
Shivam Wadhwa <shivam.wadhwa@tecdune.com>
2020-05-31 19:47:21 +0530

    Routing Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SeriesComponent } from "../app/views/series/series.component"
+import { MoviesComponent } from "../app/views/movies/movies.component"
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "" , component: MoviesComponent},
+  { path:"tvSeries", component: SeriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
