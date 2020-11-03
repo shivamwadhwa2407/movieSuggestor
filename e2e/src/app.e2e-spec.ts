ac0b17ec2ee98cce497d0ff45874b51ec7ff2846
Shivam Wadhwa <shivam.wadhwa@tecdune.com>
2020-11-04 01:10:20 +0530

    my-firstApp => nachoMovieSuggestor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-first-app app is running!');
+    expect(page.getTitleText()).toEqual('nacho-movie-suggestor app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-app'`, () => {
+  it(`should have as title 'nacho-movie-suggestor'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-first-app');
+    expect(app.title).toEqual('nacho-movie-suggestor');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-first-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('nacho-movie-suggestor app is running!');
   });
 });
